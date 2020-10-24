962a9873d3fb4f72aad7ae16c3404481301a38b1
Will Christerson <wbchristerson@gmail.com>
2020-10-24 06:37:48 -0400

    fix: Use only host prefix

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-9xo5gdfc.us.auth0.com', // the auth0 domain prefix
+    url: 'dev-9xo5gdfc.us', // the auth0 domain prefix
     audience: 'coffee', // the audience set for the auth0 app
     clientId: '3TS9Jrdx4jSTQJsm66KxdNkoxUseCd0p', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
